6ecef9bec4b110c349f0dce648191b2a9932b8f1
Shree Bhagwat <shreebhagwat94@gmail.com>
2022-04-22 15:21:43 +0530

    Added model class

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_muc_api/Models/mcu_model.dart';
+import 'package:flutter_muc_api/Models/model.dart';
 import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
@@ -13,9 +14,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   var marvelApiUrl = "https://mcuapi.herokuapp.com/api/v1/movies";
-  List<McuModels> mcuMoviesList = [];
+  List<MCUDATA?> mcuMoviesList = [];
 
   @override
   void initState() {
@@ -27,66 +27,52 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
-      body: mcuMoviesList.isNotEmpty ?
-          GridView.builder(
-            itemCount: mcuMoviesList.length,
-            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 200,
-              childAspectRatio: 2/3,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10
-            ),
-            itemBuilder: (BuildContext context, int index){
-              return
-
-                ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-
-                child:
-                mcuMoviesList[index].coverUrl != null ?
-                CachedNetworkImage(
-                  imageUrl: mcuMoviesList[index].coverUrl.toString(),
-                  placeholder: (context, url) => Image.asset('images/place_holder.png'),
-                ) : Image.asset('images/place_holder.png')
-              );
-            },
-          )
-          : Center(
-        child:Container(
-          width: 50,
-          height: 50,
-          child: Center(
-            child: CircularProgressIndicator(color: Colors.white70,),
-          ),
-        )
-      )
-
-
-    );
+        backgroundColor: Colors.grey[900],
+        body: mcuMoviesList.isNotEmpty
+            ? GridView.builder(
+                itemCount: mcuMoviesList.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 2 / 3,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10),
+                itemBuilder: (BuildContext context, int index) {
+                  return ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: mcuMoviesList[index]!.coverUrl != null
+                          ? CachedNetworkImage(
+                              imageUrl:
+                                  mcuMoviesList[index]!.coverUrl.toString(),
+                              placeholder: (context, url) =>
+                                  Image.asset('images/place_holder.png'),
+                            )
+                          : Image.asset('images/place_holder.png'));
+                },
+              )
+            : Center(
+                child: Container(
+                width: 50,
+                height: 50,
+                child: Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.white70,
+                  ),
+                ),
+              )));
   }
 
-  void getMarvelMovies(){
-    debugPrint('=========== Function running =============');
-
+  void getMarvelMovies() {
     final uri = Uri.parse(marvelApiUrl);
-      http.get(uri).then((response){
-        if(response.statusCode == 200){
-            final responseBody  = response.body;
-            final decodedData = jsonDecode(responseBody);
-            final List marvelData = decodedData['data'];
-            for(var i = 0; i < marvelData.length; i++){
-              final mcuMovie = McuModels.fromJson(marvelData[i] as Map <String, dynamic>);
-              mcuMoviesList.add(mcuMovie);
-
-            }
-            setState(() {});
-
-        }else{
-
-        }
-      }).catchError((err){
-        debugPrint('=========== $err =============');
-      });
+    http.get(uri).then((response) {
+      if (response.statusCode == 200) {
+        final responseBody = response.body;
+        final decodedData = jsonDecode(responseBody);
+        mcuMoviesList = mcg_model.fromJson(decodedData).data as List<MCUDATA?>;
+        print(mcuMoviesList.first!.id);
+        setState(() {});
+      } else {}
+    }).catchError((err) {
+      debugPrint('=========== $err =============');
+    });
   }
 }
